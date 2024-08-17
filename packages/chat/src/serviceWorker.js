5b03ebd80275b127f2458ca9f17344fa18bf550e
ydideh810 <yd.baller810@gmail.com>
2024-08-16 21:56:19 -0400

    Update serviceWorker.js

--- packages/chat/src/serviceWorker.js
+++ packages/chat/src/serviceWorker.js
@@ -1,38 +1,26 @@
-// This optional code is used to register a service worker.
-// register() is not called by default.
-
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
-
-// To learn more about the benefits of this model and instructions on how to
-// opt-in, read https://bit.ly/CRA-PWA
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
   window.location.hostname === '[::1]' || // IPv6 localhost
   window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/) // IPv4 localhost
-)
+);
 
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready
       .then((registration) => {
-        registration.unregister()
+        registration.unregister();
       })
       .catch((error) => {
-        console.error(error.message)
-      })
+        console.error(error.message);
+      });
   }
 }
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-      return // Prevent registration if origin doesn't match
+      return; // Prevent registration if origin doesn't match
     }
 
     // Function declarations moved to the root of the function body
@@ -41,73 +29,72 @@ export function register(config) {
         .register(swUrl)
         .then((registration) => {
           registration.onupdatefound = () => {
-            const installingWorker = registration.installing
+            const installingWorker = registration.installing;
             if (installingWorker == null) {
-              return
+              return;
             }
             installingWorker.onstatechange = () => {
               if (installingWorker.state === 'installed') {
                 if (navigator.serviceWorker.controller) {
                   console.log(
                     'New content is available and will be used when all tabs for this page are closed. See https://bit.ly/CRA-PWA.'
-                  )
+                  );
                   if (configParam && configParam.onUpdate) {
-                    configParam.onUpdate(registration)
+                    configParam.onUpdate(registration);
                   }
                 } else {
-                  console.log('Content is cached for offline use.')
+                  console.log('Content is cached for offline use.');
                   if (configParam && configParam.onSuccess) {
-                    configParam.onSuccess(registration)
+                    configParam.onSuccess(registration);
                   }
                 }
               }
-            }
-          }
+            };
+          };
         })
         .catch((error) => {
-          console.error('Error during service worker registration:', error)
-        })
-    }
+          console.error('Error during service worker registration:', error);
+        });
+    };
 
     const checkValidServiceWorker = (swUrl, configParam) => {
       fetch(swUrl, {
         headers: { 'Service-Worker': 'script' },
       })
         .then((response) => {
-          const contentType = response.headers.get('content-type')
+          const contentType = response.headers.get('content-type');
           if (
             response.status === 404 ||
             (contentType != null && contentType.indexOf('javascript') === -1)
           ) {
             navigator.serviceWorker.ready.then((registration) => {
               registration.unregister().then(() => {
-                window.location.reload()
-              })
-            })
+                window.location.reload();
+              });
+            });
           } else {
-            registerValidSW(swUrl, configParam)
+            registerValidSW(swUrl, configParam);
           }
         })
         .catch(() => {
-          console.log('No internet connection found. App is running in offline mode.')
-        })
-    }
+          console.log('No internet connection found. App is running in offline mode.');
+        });
+    };
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        checkValidServiceWorker(swUrl, config)
+        checkValidServiceWorker(swUrl, config);
 
         navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service worker. To learn more, visit https://bit.ly/CRA-PWA'
-          )
-        })
+          );
+        });
       } else {
-        registerValidSW(swUrl, config)
+        registerValidSW(swUrl, config);
       }
-    })
+    });
   }
 }
-
